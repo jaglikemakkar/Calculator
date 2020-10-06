4fedb70145c29cd43a0d2d87ba9dcdc833657b2a
jaglikemakkar <58853798+jaglikemakkar@users.noreply.github.com>
2020-10-07 03:52:54 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,10 @@ function btnpress(val){
     if (val==='C'){
         document.getElementById("display").value = '';
     }
+    else if(val=='b'){
+        var exp = document.getElementById("display").value;
+        document.getElementById("display").value = exp.slice(0, exp.length-1);
+    }
     else if (val === '='){
         try{
             var result = eval(document.getElementById("display").value);
